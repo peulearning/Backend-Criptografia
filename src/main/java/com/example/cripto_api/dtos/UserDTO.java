42e476f6c2a7006ec0aef80f551c81bca29cd279
Pedro <peuhenry2018@gmail.com>
2025-09-27 23:21:48 -0300

    UserDTO changes ✨

--- src/main/java/com/example/cripto_api/dtos/UserDTO.java
+++ src/main/java/com/example/cripto_api/dtos/UserDTO.java
@@ -1,5 +1,33 @@
 package com.example.cripto_api.dtos;
 
 public class UserDTO {
-  
+    private String userDocument;
+    private String creditCardToken;
+    private Long value;
+
+    public String getUserDocument() {
+        return userDocument;
+    }
+
+    public void setUserDocument(String userDocument) {
+        this.userDocument = userDocument;
+    }
+
+    public String getCreditCardToken() {
+        return creditCardToken;
+    }
+
+    public void setCreditCardToken(String creditCardToken) {
+        this.creditCardToken = creditCardToken;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+    
+
 }
